f64a2f9d3825a357eb80eb709ff7c483e8f3b85e
Uthmanbello <uthmanbello88@gmail.com>
2023-05-27 17:28:11 +0100

    Setup items author_id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,6 +65,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :amount, :user_id, :bill_id)
+      params.require(:item).permit(:name, :amount, :author_id, :bill_id)
     end
 end
