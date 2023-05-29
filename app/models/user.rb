0166c75092dc87b01e4d17e7e3d31f2f82d1d394
Uthmanbello <uthmanbello88@gmail.com>
2023-05-29 17:44:49 +0100

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :bills
   has_many :items
+
+  validates :name, presence: true
 end
